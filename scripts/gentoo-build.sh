5f86ae1f4d1bad1ee534e1abe09f73fa24cc2799
Yu Gu <guyu2876@gmail.com>
2022-09-23 21:55:21 +0800

    修改Makefile,添加gentoo-build.sh

---
+++ scripts/gentoo-build.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+set -e
+workdir=$(dirname $(dirname $(readlink -f "$0")))
+cd $workdir
+
+cd $workdir/hackernel/user-space/build/
+cmake $workdir/hackernel/user-space/ -DCMAKE_BUILD_TYPE=Release
+make
+
+cd $workdir/uranus
+make
