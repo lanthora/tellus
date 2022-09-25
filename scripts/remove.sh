cfb3f108afa6b4bbb9487c1e6b8f704e2a1fb606
lanthora <lanthora@outlook.com>
2022-09-25 15:05:33 +0000

    修复删除脚本中文件路径错误

--- scripts/remove.sh
+++ scripts/remove.sh
@@ -5,16 +5,16 @@ workdir=$(dirname $(dirname $(readlink -f "$0")))
 cd $workdir/hackernel/kernel-space/
 make remove
 
-rm /usr/sbin/hackernel
-rm /usr/sbin/uranus-sample
-rm /usr/sbin/uranus-telegram
-rm /usr/sbin/uranus-web
+rm /usr/bin/hackernel
+rm /usr/bin/uranus-sample
+rm /usr/bin/uranus-telegram
+rm /usr/bin/uranus-web
 
 rm /etc/hackernel/telegram.yaml
 rm /etc/hackernel/web.yaml
 rmdir /etc/hackernel
 
 rm /etc/modules-load.d/hackernel.conf
-rm /etc/systemd/system/hackernel.service
-rm /etc/systemd/system/uranus-telegram.service
-rm /etc/systemd/system/uranus-web.service
+rm /usr/lib/systemd/system/hackernel.service
+rm /usr/lib/systemd/system/uranus-telegram.service
+rm /usr/lib/systemd/system/uranus-web.service
