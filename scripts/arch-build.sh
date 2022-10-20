33a0566efc80ab405346135d1917e4b61486f097
lanthora <lanthora@outlook.com>
2022-10-20 15:20:00 +0000

    Arch Linux 不打包前端资源

--- scripts/arch-build.sh
+++ scripts/arch-build.sh
@@ -7,12 +7,6 @@ cd $workdir/hackernel/user-space/build/
 cmake $workdir/hackernel/user-space/ -DCMAKE_BUILD_TYPE=Release
 make
 
-cd $workdir/venus
-npm install
-npm run build
-rm -rf $workdir/uranus/internal/web/webui/*
-cp -r $workdir/venus/build/* $workdir/uranus/internal/web/webui/
-
 cd $workdir/uranus
 make init
 make
