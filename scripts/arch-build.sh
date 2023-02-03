62ac2a5534f1f61f44d2e0d9d9720a3be6f0d507
lanthora <lanthora@outlook.com>
2023-02-03 08:40:42 +0000

    仅在内存大于 4G 的时候编译前端资源

--- scripts/arch-build.sh
+++ scripts/arch-build.sh
@@ -6,11 +6,15 @@ cd $workdir/hackernel/user-space/build/
 cmake $workdir/hackernel/user-space/ -DCMAKE_BUILD_TYPE=Release
 make
 
-cd $workdir/venus
-npm install
-npm run build
-rm -rf $workdir/uranus/internal/web/webui/*
-cp -r $workdir/venus/build/* $workdir/uranus/internal/web/webui/
+# 仅在内存大于 4G 的时候编译前端资源
+if [ `awk '($1 == "MemAvailable:"){print int($2/1048576)}' /proc/meminfo` -ge 4 ]
+then
+        cd $workdir/venus
+        npm install
+        npm run build
+        rm -rf $workdir/uranus/internal/web/webui/*
+        cp -r $workdir/venus/build/* $workdir/uranus/internal/web/webui/
+fi
 
 cd $workdir/uranus
 make init
