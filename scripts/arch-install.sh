3dfbc02d03c73fd35b57fc1156bfba150745fd82
lanthora <lanthora@outlook.com>
2023-03-11 01:06:57 +0800

    添加 uranus-notify

--- scripts/arch-install.sh
+++ scripts/arch-install.sh
@@ -13,14 +13,18 @@ cp $workdir/hackernel/user-space/build/hackernel $DESTDIR/usr/bin/hackernel
 cp $workdir/uranus/cmd/sample/uranus-sample $DESTDIR/usr/bin/uranus-sample
 cp $workdir/uranus/cmd/telegram/uranus-telegram $DESTDIR/usr/bin/uranus-telegram
 cp $workdir/uranus/cmd/web/uranus-web $DESTDIR/usr/bin/uranus-web
+cp $workdir/uranus/cmd/notify/uranus-notify $DESTDIR/usr/bin/uranus-notify
 
 mkdir -p $DESTDIR/etc/hackernel/
 cp $workdir/uranus/configs/telegram.yaml $DESTDIR/etc/hackernel/telegram.yaml
 cp $workdir/uranus/configs/web.yaml $DESTDIR/etc/hackernel/web.yaml
+cp $workdir/uranus/configs/notify.yaml $DESTDIR/etc/hackernel/notify.yaml
 
 mkdir -p $DESTDIR/etc/modules-load.d/
 mkdir -p $DESTDIR/usr/lib/systemd/system/
+mkdir -p $DESTDIR/usr/lib/systemd/user/
 cp $workdir/hackernel/scripts/modules-load/hackernel.conf $DESTDIR/etc/modules-load.d/hackernel.conf
 cp $workdir/hackernel/scripts/systemd/hackernel.service $DESTDIR/usr/lib/systemd/system/hackernel.service
 cp $workdir/uranus/init/uranus-telegram.service $DESTDIR/usr/lib/systemd/system/uranus-telegram.service
 cp $workdir/uranus/init/uranus-web.service $DESTDIR/usr/lib/systemd/system/uranus-web.service
+cp $workdir/uranus/init/uranus-notify.service $DESTDIR/usr/lib/systemd/user/uranus-notify.service
--- scripts/install.sh
+++ scripts/install.sh
@@ -9,6 +9,7 @@ cp $workdir/hackernel/user-space/build/hackernel /usr/bin/hackernel
 cp $workdir/uranus/cmd/sample/uranus-sample /usr/bin/uranus-sample
 cp $workdir/uranus/cmd/telegram/uranus-telegram /usr/bin/uranus-telegram
 cp $workdir/uranus/cmd/web/uranus-web /usr/bin/uranus-web
+cp $workdir/uranus/cmd/notify/uranus-notify /usr/bin/uranus-notify
 
 mkdir -p /etc/hackernel/
 cp $workdir/uranus/configs/telegram.yaml /etc/hackernel/telegram.yaml
@@ -16,7 +17,9 @@ cp $workdir/uranus/configs/web.yaml /etc/hackernel/web.yaml
 
 mkdir -p /etc/modules-load.d/
 mkdir -p /usr/lib/systemd/system/
+mkdir -p /usr/lib/systemd/user/
 cp $workdir/hackernel/scripts/modules-load/hackernel.conf /etc/modules-load.d/hackernel.conf
 cp $workdir/hackernel/scripts/systemd/hackernel.service /usr/lib/systemd/system/hackernel.service
 cp $workdir/uranus/init/uranus-telegram.service /usr/lib/systemd/system/uranus-telegram.service
 cp $workdir/uranus/init/uranus-web.service /usr/lib/systemd/system/uranus-web.service
+cp $workdir/uranus/init/uranus-notify.service /usr/lib/systemd/user/uranus-notify.service
